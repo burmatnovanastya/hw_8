be2b542d3f60b000926af38ef2af5d03f7dfc40b
burmatnovanastya <burmatnova.nastya@mail.ru>
2021-10-08 09:40:00 +0300

    new file

---
+++ 1.py
@@ -0,0 +1,17 @@
+def convert(num, base):
+	result = ''
+	while num > 0:
+		result = str(num % base) + result
+		num = num // base
+	print(result)
+
+try: 
+	a = int(input('Введите число: '))
+	b = int(input('Введите целевую систему счисления: '))
+	if a > 0 and b == 2 or b == 8:
+		convert(a, b)
+	else:
+		print('Ошибка входных данных.')
+
+except ValueError :
+	print('Ошибка. Введите целое число.')
